03de7159b8286135b03518bcd8f6cbc5c5719d53
kaheath <kaheath@ncsu.edu>
2025-09-24 19:37:48 -0400

    fixed good guy/bad guy

--- TextAdventure/Board.py
+++ TextAdventure/Board.py
@@ -15,7 +15,8 @@ class Board:
         screen.setup(self.board_width + 200, self.board_height + 200)
         turtle.speed(0)
         self.draw_board()
-        self.draw_bad_guy(self.board_width + 250, self.board_height + 250, 30)
+        self.draw_bad_guy(1, 1, 30)
+        self.draw_good_guy(2, 2, 30)
         turtle.done()
     
     def draw_board(self):
@@ -34,12 +35,23 @@ class Board:
             turtle.fd(self.square_size)
             turtle.left(90)
     
-    def draw_bad_guy(self, x, y, radius):
+    def draw_bad_guy(self, row, col, radius):
+        # compute center of grid cell
+        x = -self.board_width // 2 + col * self.square_size + self.square_size // 2
+        y = -self.board_height // 2 + row * self.square_size + self.square_size // 2
+        turtle.up()
+        turtle.goto(x, y - radius)  # offset so circle is centered
+        turtle.down()
         turtle.color("red")
         turtle.circle(radius)
         turtle.color("black")
 
-    def draw_good_guy(self, x, y, radius):
+    def draw_good_guy(self, row, col, radius):
+        x = -self.board_width // 2 + col * self.square_size + self.square_size // 2
+        y = -self.board_height // 2 + row * self.square_size + self.square_size // 2
+        turtle.up()
+        turtle.goto(x, y - radius)
+        turtle.down()
         turtle.color("yellow")
         turtle.circle(radius)
         turtle.color("black")
