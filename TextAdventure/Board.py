76f78bb6d194ac46fc899b6e188603d96a5d001b
Jason Yin <jyin23@ncsu.edu>
2025-10-08 20:32:06 -0400

    loading board/map, draw board refactor, starting on character/player
    Co-authored-by: kayheath <kayheath@users.noreply.github.com>
    Co-authored-by: Riana Shruti Victoria <rsvictor+ncstate@ncsu.edu>

--- TextAdventure/Board.py
+++ TextAdventure/Board.py
@@ -1,11 +1,17 @@
 import turtle
-import Character
+# import Character
+import Tile
 
 class Board:
-    def __init__(self, initialBoard):
-        self.boardState = initialBoard
-        self.rows = len(initialBoard)
-        self.cols = len(initialBoard[0])
+    def __init__(self, path):
+        self.boardState = []
+        self.good = (-1, -1)
+        self.bad = (-1, -1)
+        self.load_board(path)
+        self.rows = len(self.boardState)
+        self.cols = len(self.boardState[0])
+
+        # print(self.rows, self.cols)
         self.bad_guy_turtle = turtle.Turtle()
         self.good_guy_turtle = turtle.Turtle()
         self.square_size = 75
@@ -16,9 +22,37 @@ class Board:
         screen.setup(self.board_width + 200, self.board_height + 200)
         turtle.speed(0)
         self.draw_board()
-        self.draw_bad_guy(1, 1)
-        self.draw_good_guy(2, 2)
+        self.draw_bad_guy(self.bad[0], self.bad[1])
+        self.draw_good_guy(self.good[0], self.good[1])
         turtle.done()
+    
+    def get_tile(self, row, col):
+        return self.boardState[row][col]
+    
+    def load_board(self, path):
+        with open(path, "r") as fin:
+            print(self.good, self.bad)
+            lines = fin.readlines()
+            boardState = []
+            for (lineNumber, line) in enumerate(lines):
+                currentRow = []
+                numbers = list(map(str, line.strip().split()))
+                # print(numbers)
+                for (colNumber, num) in enumerate(numbers):
+                    status = int(num[0])
+                    if len(num) > 1:
+                        entity = num[1]
+                        if entity == "g":
+                            self.good = (lineNumber, colNumber)
+                        if entity == "b":
+                            self.bad = (lineNumber, colNumber)
+                        t = Tile.Tile(status, entity)
+                    else:
+                        t = Tile.Tile(status, None)
+                    currentRow.append(t)
+                boardState.append(currentRow)
+            # print(boardState[0][0].getStatus())
+            self.boardState = boardState
     
     def draw_board(self):
         for i in range(self.rows):
@@ -26,7 +60,7 @@ class Board:
                 # offset so board is centered
                 x = -self.board_width // 2 + j * self.square_size
                 y = -self.board_height // 2 + i * self.square_size
-                self.draw_square(x, y, self.boardState[i][j])
+                self.draw_square(x, y, self.boardState[i][j].getStatus())
         
     def draw_square(self, x, y, fill):
         if (fill):
@@ -56,8 +90,10 @@ class Board:
         t1.up()
         t1.goto(x, y - 30)  # offset so circle is centered
         t1.down()
+        turtle.begin_fill()
         t1.color("red")
         t1.circle(30)
+        turtle.end_fill()
         t1.color("black")
 
     def draw_good_guy(self, row, col):
@@ -66,17 +102,21 @@ class Board:
         turtle.up()
         turtle.goto(x, y - 30)
         turtle.down()
-        turtle.color("yellow")
+        turtle.begin_fill()
+        turtle.color("green")
         turtle.circle(30)
+        turtle.end_fill()
         turtle.color("black")
 
-test = [[1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1]]
 
-Board(test)
+
+# test = [[1, 1, 1, 1, 1, 1, 1, 1],
+#         [1, 0, 0, 0, 0, 0, 0, 1],
+#         [1, 0, 0, 0, 0, 0, 0, 1],
+#         [1, 0, 0, 0, 0, 0, 0, 1],
+#         [1, 0, 0, 0, 0, 0, 0, 1],
+#         [1, 0, 1, 0, 0, 0, 0, 1],
+#         [1, 0, 1, 0, 0, 0, 0, 1],
+#         [1, 1, 1, 1, 1, 1, 1, 1]]
+
+b = Board("maps/map1.txt")
--- TextAdventure/Character.py
+++ TextAdventure/Character.py
@@ -1,9 +1,11 @@
 from item.HealthPotion import *
 
 class Character:
-    def __init__(self, current_hp, max_hp):
+    def __init__(self, current_hp, max_hp, initX, initY):
         self.hp = [current_hp, max_hp]
         self.inventory = {"items": [HealthPotion(max_hp / 2, 1)]}
+        self.xPos = initX
+        self.yPos = initY
     
     # Heals the Character if they have a HealthPotion in their inventory, upto their max health. Returns the amount of health healed
     def heal(self):
