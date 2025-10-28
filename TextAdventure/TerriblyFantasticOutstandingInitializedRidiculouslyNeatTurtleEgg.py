0e401afb1c49c5774b72d0cb083e09ac5cdaa4f9
Carlos Martinez Poteet <cmmart26@ncsu.edu>
2025-10-28 12:43:04 -0400

    Added player movment, made itemLoading a function, updated board to not draw board until called

--- TextAdventure/TerriblyFantasticOutstandingInitializedRidiculouslyNeatTurtleEgg.py
+++ TextAdventure/TerriblyFantasticOutstandingInitializedRidiculouslyNeatTurtleEgg.py
@@ -3,8 +3,10 @@ import turtle
 from board import Board
 import characters
 import items
+from ItemLoading import initalizePlayer
 
 MAP_1_PATH = "maps/map1.txt"
+status = ''
 
 def state_checks():
     for badGuy in badGuys:
@@ -12,14 +14,36 @@ def state_checks():
             # Enter combat
             print("In Combat")
 
-    if (player.get_hp[0] == 0):
+    if (player.get_hp() == 0):
         player.die()
         print("Player Died")
-        return 
+        return -1
 
 def up():
+    print('Moving up')
     player.move_up()
-    player._draw_self()
+    player._draw_self('blue')
+    status = state_checks()
+    screen.update()
+
+def down():
+    print('Moving down')
+    player.move_down()
+    player._draw_self('blue')
+    status = state_checks()
+    screen.update()
+
+def right():
+    print('Moving right')
+    player.move_right()
+    player._draw_self('blue')
+    status = state_checks()
+    screen.update()
+
+def left():
+    print('Moving left')
+    player.move_left()
+    player._draw_self('blue')
     status = state_checks()
     screen.update()
 
@@ -29,14 +53,18 @@ def up():
 screen = turtle.Screen()
 screen.tracer(0)
 
-board = Board.Board("maps/map1.txt")
+board = Board.Board(MAP_1_PATH)
 badGuys = []
 loot = []
-player = characters.Player()
+player = initalizePlayer()
 
+board.draw_board()
 screen.listen()
 
 screen.onkey(up, 'Up')
+screen.onkey(down, 'Down')
+screen.onkey(right, 'Right')
+screen.onkey(left, 'Left')
 
 
 
