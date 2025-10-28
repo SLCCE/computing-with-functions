da52cd30c20e82277190a4556c3ac3e9386c4d42
Carlos Martinez Poteet <cmmart26@ncsu.edu>
2025-10-28 11:58:20 -0400

    gameplayloop outline

--- TextAdventure/TerriblyFantasticOutstandingInitializedRidiculouslyNeatTurtleEgg.py
+++ TextAdventure/TerriblyFantasticOutstandingInitializedRidiculouslyNeatTurtleEgg.py
@@ -4,10 +4,43 @@ from board import Board
 import characters
 import items
 
+MAP_1_PATH = "maps/map1.txt"
 
-b = Board.Board("maps/map1.txt")
-print(b)
+def state_checks():
+    for badGuy in badGuys:
+        if badGuy.get_position() == player.get_position():
+            # Enter combat
+            print("In Combat")
+
+    if (player.get_hp[0] == 0):
+        player.die()
+        print("Player Died")
+        return 
+
+def up():
+    player.move_up()
+    player._draw_self()
+    status = state_checks()
+    screen.update()
 
 
-print("Hello World!")   
 
+
+screen = turtle.Screen()
+screen.tracer(0)
+
+board = Board.Board("maps/map1.txt")
+badGuys = []
+loot = []
+player = characters.Player()
+
+screen.listen()
+
+screen.onkey(up, 'Up')
+
+
+
+screen.mainloop()
+
+
+    
